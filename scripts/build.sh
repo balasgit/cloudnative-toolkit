3419e68cf9a3b49abf2e905755463d4cd4babd5d
Matthew Perrins <mjperrin@us.ibm.com>
2020-05-11 22:17:39 -0500

    Release 0.0.2

--- scripts/build.sh
+++ scripts/build.sh
@@ -36,6 +36,7 @@ echo "Copying from ${SRC_DIR} to ${WORKSPACE_DIR}"
 cp "${SRC_DIR}/${STAGES_DIRECTORY}/variables.tf" "${WORKSPACE_DIR}"
 cp "${SRC_DIR}/${STAGES_DIRECTORY}"/stage*.tf "${WORKSPACE_DIR}"
 cp "${SRC_DIR}"/scripts/* "${WORKSPACE_DIR}"
+cp docs/README.MD ${WORKSPACE_DIR}
 
 release-it patch ${PRE_RELEASE} --ci --no-npm --no-git.push --no-git.requireCleanWorkingDir                       --verbose                       -VV
 
