1c5241823c700926006aa079b5f68a06d6c6145b
Matthew Perrins <mjperrin@us.ibm.com>
2020-06-01 15:37:40 -0500

    Latest Iteration Zero Terraform stages

--- stage0.tf
+++ stage0.tf
@@ -1,6 +1,3 @@
 terraform {
   required_version = "> 0.12.0"
-  backend "local" {
-    path = "./state/terraform.tfstate"
-  }
 }
--- stage2-argocd.tf
+++ stage2-argocd.tf
@@ -1,5 +1,5 @@
 module "dev_tools_argocd" {
-  source = "github.com/ibm-garage-cloud/terraform-tools-argocd.git?ref=v2.1.1"
+  source = "github.com/ibm-garage-cloud/terraform-tools-argocd.git?ref=v2.2.0"
 
   cluster_config_file = module.dev_cluster.config_file_path
   cluster_type        = module.dev_cluster.type_code
