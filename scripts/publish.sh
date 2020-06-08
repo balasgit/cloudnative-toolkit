d1bfb54c0467a12f29dcc74495e264aa56e6c0cb
Matthew Perrins <mjperrin@us.ibm.com>
2020-06-08 14:39:55 -0500

    Latest Iteration Zero Terraform stages

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,8 +10,8 @@ release-it --ci --no-npm --no-git.push --no-git.requireCleanWorkingDir --verbose
 
 # Setup Offering Registration Command with current version included
 VERSION=$(eval git describe --tags --abbrev=0)
-cat scripts/offering.sh | sed "s/VERSION=/VERSION=${VERSION}/g" > ./offering.sh
-cp scripts/offering.json .
+cat scripts/master.sh | sed "s/VERSION=/VERSION=${VERSION}/g" > ./offering.sh
+cp scripts/master.json ./offering.json
 chmod +x offering.sh
 git add .
 
