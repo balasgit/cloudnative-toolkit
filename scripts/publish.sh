0ed93bebe7cdedbebe205d7704f15f9b29b66738
Sean Sundberg <seansund@us.ibm.com>
2020-06-09 23:03:06 -0500

    Updates publish script to use release-it hooks

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,21 +3,9 @@ echo 'Publish Release of Tile'
 
 set -e
 
-npm i
-
-git add .
+npm i -g release-it
 
 # Release the Version
-release-it --ci --no-npm --no-git.push --no-git.requireCleanWorkingDir --verbose -VV
-
-# Setup Offering Registration Command with current version included
-VERSION=$(eval git describe --tags --abbrev=0)
-cat scripts/master.sh | sed "s/VERSION=/VERSION=${VERSION}/g" > ./offering.sh
-cp scripts/master.json ./offering.json
-chmod +x offering.sh
-git add .
-
-git commit -m "Latest Iteration Zero Terraform stages"
-git push --follow-tags -v
+release-it --ci minor --verbose -VV
 
 echo 'Release complete .......'
